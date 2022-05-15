813769d75f145f9e12557580501ce3dce79d30d6
xiexiong <alex.xie1@usmarthk.com>
2022-05-15 15:50:55 +0800

    联合类型

--- demo/union-types.ts
+++ demo/union-types.ts
@@ -2,3 +2,19 @@
 let num: number|string
 num = 22
 num = '222'
+
+
+// 交叉类型
+// 如果要对对象形状进行扩展，可以使用交叉类型 &
+interface Test {
+    name: string,
+    age: number
+}
+
+type Student22 = Test & {grade: number}
+
+const student1:Student22 = {
+    name: '11',
+    age: 11,
+    grade:2
+}
